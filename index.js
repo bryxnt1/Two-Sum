8c1c213de13ee6f74c1bdf10b2ef8e5815a6d2af
bryxnt1 <bryantperez56@gmail.com>
2023-03-05 09:45:37 -0500

    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  const hashTable = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    
+    if (hashTable[complement] !== undefined) {
+      return [hashTable[complement], i];
+    }
+    
+    hashTable[nums[i]] = i;
+  }
+  
+  return [];
+}
